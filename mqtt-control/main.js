f3e12a99a70e9ebb2c3bc33683b4ae9448590224
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2023-08-10 17:43:14 +0530

    Login

--- mqtt-control/main.js
+++ mqtt-control/main.js
@@ -1,4 +1,6 @@
 const db = require('../db');
+const jwtUtils = require('./jwtUtils');
+const bcrypt = require('bcrypt');
 
 function fetchStatus(req, res) {
   const deviceStatusQuery = 'select * from ahu_control order by date_time DESC LIMIT 1';
@@ -17,53 +19,6 @@ function fetchStatus(req, res) {
   }
 }
 
-/*function fetchOnOffTimings(req, res) {
-  const deviceStatusQuery = 'select * from ahu_control order by date_time ASC';
-  try {
-    db.query(deviceStatusQuery, (error, data) => {
-      if (error) {
-        console.error('Error during data fetch:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-      }
-
-      const onOffTimings = [];
-      let previousStatus = null;
-      let previousTimestamp = null;
-
-      for (const entry of data) {
-        if (entry.ledState === 'on' && previousStatus === 'off') {
-          const timeDifference = new Date(entry.date_time) - previousTimestamp;
-          onOffTimings.push(timeDifference);
-        }
-        previousStatus = entry.ledState;
-        previousTimestamp = new Date(entry.date_time);
-      }
-
-      const totalOnTime = onOffTimings
-        .filter((interval, index) => index % 2 === 0)
-        .reduce((acc, val) => acc + val, 0);
-
-      const totalOffTime = onOffTimings
-        .filter((interval, index) => index % 2 !== 0)
-        .reduce((acc, val) => acc + val, 0);
-
-      const formatTime = milliseconds => {
-        const seconds = Math.floor(milliseconds / 1000);
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds % 3600) / 60);
-        return `${hours}h ${minutes}m ${seconds % 60}s`;
-      };
-
-      res.status(200).json({
-        on: formatTime(totalOnTime),
-        off: formatTime(totalOffTime)
-      });
-    });
-  } catch (error) {
-    console.error('Error in data fetch:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-}*/
 function fetchOnOffTimings(req, res) {
   const deviceStatusQuery = 'select * from ahu_control order by date_time ASC';
   try {
@@ -105,9 +60,130 @@ function fetchOnOffTimings(req, res) {
   }
 }
 
+function login(req, res) {
+  const { Username, Password } = req.body;
 
+  // Check if the user exists in the database
+  const query = 'SELECT * FROM ahu_users WHERE Username = ?';
+  db.query(query, [Username], (error, rows) => {
+    try {
+      if (error) {
+        throw new Error('Error during login');
+      }
+
+      if (rows.length === 0) {
+        return res.status(401).json({ message: 'User does not exist!' });
+      }
+
+      const user = rows[0];
+
+      if (user.is_active === '1') {
+        return res.status(401).json({ message: 'User is already loggedIN. Please logout first!' });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      bcrypt.compare(Password, user.Password, (error, isPasswordValid) => {
+        try {
+          if (error) {
+            throw new Error('Error during password comparison');
+          }
+
+          if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+          }
+
+          // Generate a JWT token
+          const token = jwtUtils.generateToken({ Username: user.Username });
+          res.json({ token });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: 'Internal server error' });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+}
+
+function register(req, res) {
+  const {
+    personalEmail,
+    password,
+  } = req.body;
+
+  try {
+
+    // Check if the username (company email) is already registered
+    const personalEmailCheckQuery = 'SELECT * FROM ahu_users WHERE Username = ?';
+    db.query(personalEmailCheckQuery, [personalEmail], (error, personalEmailCheckResult) => {
+      if (error) {
+        console.error('Error during username check:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      try {
+        if (personalEmailCheckResult.length > 0) {
+          console.log('Username already exists');
+          return res.status(400).json({ message: 'User already exists' });
+        }
+
+        // Hash the password
+        bcrypt.hash(password, 10, (error, hashedPassword) => {
+          if (error) {
+            console.error('Error during password hashing:', error);
+            return res.status(500).json({ message: 'Internal server error' });
+          }
+
+          try {
+            // Generate a verification token
+            const verificationToken = jwtUtils.generateToken({ personalEmail: personalEmail });
+
+            // Insert the user into the database
+            const insertQuery =
+              'INSERT INTO ahu_users (Username, Password, is_active) VALUES (?, ?, ?)';
+            db.query(
+              insertQuery,
+              [
+                personalEmail,
+                hashedPassword,
+                '0'
+              ],
+              (error, insertResult) => {
+                if (error) {
+                  console.error('Error during user insertion:', error);
+                  return res.status(500).json({ message: 'Internal server error' });
+                }
+
+                try {
+                  console.log('User registered successfully');
+                  res.json({ message: 'Registration successful. Check your email for the verification token.' });
+                } catch (error) {
+                  console.error('Error sending verification token:', error);
+                  res.status(500).json({ message: 'Internal server error' });
+                }
+              }
+            );
+          } catch (error) {
+            console.error('Error during registration:', error);
+            res.status(500).json({ message: 'Internal server error' });
+          }
+        });
+      } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
 
 module.exports = {
   fetchStatus,
-  fetchOnOffTimings
+  fetchOnOffTimings,
+  login,
+  register
 };
--- routes.js
+++ routes.js
@@ -5,5 +5,7 @@ const main = require('./mqtt-control/main');
 
 router.get('/status', main.fetchStatus);
 router.get('/time', main.fetchOnOffTimings);
+router.post('/login', main.login);
+router.post('/register', main.register);
 
 module.exports = router;
