dd1a573e2630ab22fe6817d4f3012c87bba41300
GauravJadhav33 <gjadhav4222.com>
2023-08-26 11:49:52 +0530

    fix

--- store.js
+++ store.js
@@ -1,25 +1,28 @@
+// Import required modules
 const mqtt = require('mqtt');
-const mysql = require('mysql'); // Import the MySQL module
+const mysql = require('mysql');
 
+// MQTT broker configuration
 const broker = 'mqtt://broker.emqx.io';
 const topic = 'device/info';
-
 const client = mqtt.connect(broker);
 
-let previousLedState = null;
-
 // MySQL Database Configuration
-const dbConfig = {
+const db = {
   host: 'senselivedb.cn5vfllmzwrp.ap-south-1.rds.amazonaws.com',
   user: 'admin',
-  password: 'Sense123',
+  password: 'sense123',
   database: 'AHU',
 };
+const dbConnection = mysql.createConnection(db);
 
-const dbConnection = mysql.createConnection(dbConfig);
+// Store the previous LED state
+let previousLedState = null;
 
+// MQTT client event handling
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
+  // Subscribe to the specified topic
   client.subscribe(topic, (err) => {
     if (err) {
       console.error('Error subscribing to topic:', err);
@@ -31,13 +34,12 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
   const data = JSON.parse(message.toString());
-  //console.log('Received message:', data);
 
   // Check if the LED state has changed
   if (data.ledState !== previousLedState) {
     console.log('LED state has changed:', data.ledState);
 
-    // Combine the currentDate and currentDateTime into a single datetime format
+    // Combine date and time into a single datetime format
     const dateTimeString = `${data.currentDate} ${data.currentDateTime}`;
     const date_time = new Date(dateTimeString);
 
@@ -53,7 +55,6 @@ client.on('message', (topic, message) => {
       }
     });
 
-    // Update the previous LED state with the new value
     previousLedState = data.ledState;
   }
 });
@@ -65,6 +66,6 @@ client.on('error', (err) => {
 process.on('SIGINT', () => {
   console.log('Closing MQTT client...');
   client.end();
-  dbConnection.end(); // Close the MySQL connection
+  dbConnection.end();
   process.exit();
 });
